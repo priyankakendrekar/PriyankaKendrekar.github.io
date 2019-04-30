0a35a9d594e0189fd3c28eec29d03170e97f5be7
priyanka kendrekar <priyanka.kendrekar@gmail.com>
2019-04-30 18:31:40 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "",
         
         {
         // how many entries do you want?
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+   /* GitHubCalendar("#github-graph", "IonicaBizau");*/
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
